22b4f36cc5ac5eac33994b895392a53a5c859e34
johir-rayhan <johir.rayhan01@gmail.com>
2022-02-01 09:06:18 +0600

    Fix create-update-comments

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -45,8 +45,12 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
-
+      const commentDoc = {
+        ...user,
+        movie_id: ObjectId(movieId),
+        text: comment,
+        date,
+      }
       return await comments.insertOne(commentDoc)
     } catch (e) {
       console.error(`Unable to post comment: ${e}`)
@@ -70,8 +74,8 @@ export default class CommentsDAO {
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
       const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+        { _id: ObjectId(commentId), email: userEmail },
+        { $set: { text, date } },
       )
 
       return updateResponse
